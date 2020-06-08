31872c005a89be9028183e4f4384b9eab2ed1072
dasha <frgyvu@gmail.com>
2020-06-08 22:40:53 +0300

    ++

---
+++ src/main/java/ru/ifmo/firstspring/controllers/HomeController.java
@@ -0,0 +1,35 @@
+package ru.ifmo.firstspring.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller // bean beanContainer
+public class HomeController {
+
+    @RequestMapping("/")
+    public String index(){
+        return "index";
+    }
+
+}
+
+/*
+html    контроллер(репозиторий)    данные
+
+html
+контроллер(сервис)
+сервис(репозиторий) - обработка данных
+данные
+*/
+
+/*
+клиент -> сообщение -> сервер
+клиент <- сообщение <- сервер
+
+сообщение:
+    строка запроса / статус
+    HTTP заголовки
+    тело сообщения
+
+"/" | spring
+*/
